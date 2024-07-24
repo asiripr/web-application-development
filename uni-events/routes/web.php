f1f35050171ff321595aa5b62eaee8184d64f0f8
asiripr <asiripramodaya2000@gmail.com>
2024-07-24 15:14:18 +0530

    added some frontend pages

--- uni-events/routes/web.php
+++ uni-events/routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', [HomeController::class,'about']);
+
+Route::get('/home', [HomeController::class,'home']);
+
+Route::get('/eventcreation', [AdminController::class,'eventcreation']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
